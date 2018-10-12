ee2e866293940bf2a19a9297dea583e657372ec4
Moritz Klein <moritz.klein@protonmail.com>
2018-10-12 20:59:23 +0200

    Less variable: wuuuh

--- lists_6-rearrangeLastN.py
+++ lists_6-rearrangeLastN.py
@@ -1,29 +1,25 @@
 # Definition for singly-linked list:
-class ListNode(object):
-  def __init__(self, x):
-    self.value = x
-    self.next = None
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
 
 def rearrangeLastN(l, n):
     origHead = l
     lastN = []
-    count = 0
     removeFrom = ListNode(None)
     
     if n == 0:
         return l
     
     while l is not None:
-        count += 1
         lastN.append(l)
         if len(lastN) > n:
             removeFrom = lastN.pop(0)
         l = l.next
     
-    if count == n:
-        return origHead
-    
-    lastN[n-1].next = origHead
-    removeFrom.next = None
+    if origHead is not lastN[0]:
+        lastN[n-1].next = origHead
+        removeFrom.next = None
     
     return lastN[0]
